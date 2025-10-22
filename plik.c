1472de304a84361e84d46aa656b6976aa091a80f
Artsiom Rzhevutski <arzewucki05@ultra60.mat.umk.pl>
2025-10-22 15:08:17 +0200

    Edited file

--- plik.c
+++ plik.c
@@ -3,6 +3,6 @@
 int main(void){
 	printf("I have a terrible headache today T_T\n");
 	printf("I want to go home so baaaaaaaaaaaaad\n");
-
+	printf("Some changes to complete this task\n");
 	return 0;
 }
--- plik.c~
+++ plik.c~
@@ -2,7 +2,7 @@
 
 int main(void){
 	printf("I have a terrible headache today T_T\n");
-	printf("I want to go home so baaaaaaad\n");
+	printf("I want to go home so baaaaaaaaaaaaad\n");
 
 	return 0;
 }
